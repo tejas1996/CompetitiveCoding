76c8a744cc38eb115a54d2432618b5b350ce713c
tejas1996 <tejas.dond@ves.ac.in>
2017-12-17 15:30:42 +0530

    first commit

---
+++ src/main/java/Testing.java
@@ -0,0 +1,19 @@
+public class Testing {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hey this iis fast ");
+        System.out.println("stilll fast ");
+        function();
+    }
+
+
+    public static void function(){
+
+        System.out.println("this is a function");
+
+
+    }
+}
