a1d51f8da5930384bc6683095e66bf64585ef996
tejas1996 <tejas.dond@ves.ac.in>
2017-12-22 15:26:46 +0530

    "ReadWrire Locks"

---
+++ src/main/java/LetsSee.java
@@ -0,0 +1,76 @@
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class LetsSee {
+
+    private static String apple = "apple";
+    ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final LetsSee letsSee = new LetsSee();
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    System.out.println("lock got");
+                    letsSee.change();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        t1.start();
+
+        Thread.sleep(1000);
+
+        for (int i = 0; i < 10; i++) {
+
+
+            Thread t2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    try {
+                        letsSee.read();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+            t2.start();
+
+        }
+        System.out.println("waiting");
+
+
+    }
+
+    private void change() throws InterruptedException {
+
+        readWriteLock.writeLock().lock();
+        apple = apple + "A";
+        Thread.sleep(5000);
+        //readWriteLock.notifyAll();
+        apple = apple + "E";
+        System.out.println("gettingout");
+        readWriteLock.writeLock().unlock();
+
+
+    }
+
+    private void read() throws InterruptedException {
+
+        readWriteLock.readLock().lock();
+        //    Thread.sleep(1000);
+        System.out.println(apple);
+        readWriteLock.readLock().unlock();
+
+
+    }
+
+}
