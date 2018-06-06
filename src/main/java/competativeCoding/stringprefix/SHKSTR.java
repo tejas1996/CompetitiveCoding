39f9182c007074acd82dd87e3ad254c30819bc24
tejas1996 <tejas.dond@ves.ac.in>
2018-06-07 01:11:52 +0530

    working  code

--- src/main/java/competativeCoding/stringprefix/SHKSTR.java
+++ src/main/java/competativeCoding/stringprefix/SHKSTR.java
@@ -13,8 +13,10 @@ public class SHKSTR {
         HashMap<Character, Nody> nodyHashMap = new HashMap<Character, Nody>();
         int t = sc.nextInt();
         int count = 0;
+        ArrayList<String> store = new ArrayList<String>();
         while (count < t) {
             String value = sc.next();
+            store.add(value);
             char[] array = new char[100];
             array = value.toCharArray();
 
@@ -27,13 +29,14 @@ public class SHKSTR {
 
             root = nodyHashMap.get(array[0]);
             current = root;
-
+            int flag = 0;
             for (int i = 1; i < array.length; i++) {
                 Nody chi = current.contains(array[i]);
                 if (chi != null) {
                     current.number.add(count);
                     current = chi;
                 } else {
+                    flag = 1;
                     Nody node = new Nody(array[i], count);
                     current.child.add(node);
                     current.number.add(count);
@@ -43,6 +46,11 @@ public class SHKSTR {
 
 
             }
+
+            if (flag == 0) {
+                current.number.add(count);
+            }
+
 
 
             count++;
@@ -64,10 +72,15 @@ public class SHKSTR {
 
             for (int i = 1; i < finalst.length; i++) {
 
-                if (current.child.contains(finalst[i])) {
+                Nody chi = current.contains(finalst[i]);
+                if (chi != null) {
+                    if (i == (finalst.length - 1)) {
+                        System.out.println(findLex(chi.number, store, first));
+                    }
+                    current = chi;
                     continue;
                 }
-                System.out.println(current.number.get(0));
+                System.out.println(findLex(current.number, store, first));
                 break;
 
             }
@@ -77,8 +90,22 @@ public class SHKSTR {
         }
 
 
-        System.out.println(nodyHashMap.size());
+    }
 
+    private static String findLex(ArrayList<Integer> integer, ArrayList<String> store, int first) {
+
+        // logic for lex
+
+        String min = "zzzzzzzzzzzzzzzzz";
+        for (Integer i : integer) {
+            if (min.compareTo(store.get(i)) > 0) {
+                if (i < first)
+                    min = store.get(i);
+            }
+
+
+        }
+        return min;
 
     }
 
