4668589d2d3dcb99474286d8327e9296186cc4f2
tejas1996 <tejas.dond@ves.ac.in>
2018-12-12 23:40:57 +0530

    GFG - Check for BST.

---
+++ src/main/java/algorithms/CheckBST.java
@@ -0,0 +1,49 @@
+package algorithms;
+
+public class CheckBST {
+
+    public static void main(String[] args) {
+
+        Node node = formTree();
+        boolean isIT = isBST(node, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        System.out.println(isIT);
+
+
+    }
+
+    private static boolean isBST(Node node, int minValue, int maxValue) {
+
+        if (node == null) {
+            return true;
+        }
+
+        if (node.value < minValue || node.value > maxValue) {
+            return false;
+        }
+
+        if (isBST(node.left, minValue, node.value - 1) && isBST(node.right, node.value + 1, maxValue)) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+    private static Node formTree() {
+
+        Node root = new Node(5);
+        Node three = new Node(3);
+        Node eight = new Node(8);
+        root.left = three;
+        root.right = eight;
+        Node one = new Node(1);
+        Node six = new Node(6);
+        three.left = one;
+        three.right = six;
+
+        return root;
+
+    }
+
+}
---
+++ src/main/java/algorithms/Node.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+public class Node {
+
+    int value;
+    Node left, right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+}
