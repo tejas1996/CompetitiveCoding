f3ded1ba95db585c396efa45470dcd27c18a3e84
tejas1996 <tejas.dond@ves.ac.in>
2018-12-15 13:59:45 +0530

    fixing the solution of intxor.

--- src/main/java/competativeCoding/declong/INTXOR.java
+++ src/main/java/competativeCoding/declong/INTXOR.java
@@ -15,36 +15,18 @@ public class INTXOR {
             ArrayList arrayList = new ArrayList();
             int n = scanner.nextInt();
 
-            if (n % 2 == 0) {
-                for (int i = 1; i <= n; i++) {
+            System.out.println("1 " + "1" + " " + n + " " + n);
+            int ans = scanner.nextInt();
+            arrayList.add(ans);
 
-                    int number = i + 1;
-                    System.out.println("1 " + i + " " + i + " " + number);
+            for (int i = 2; i <= n; i++) {
+
+                int number = i - 1;
+                System.out.println("1 " + i + " " + number + " " + number);
                     int ans1 = scanner.nextInt();
-                    System.out.println("1 " + i + " " + number + " " + number);
-                    int ans0 = scanner.nextInt();
-                    arrayList.add(i - 1, ans0);
-                    arrayList.add(i, ans1);
-                    i++;
+                arrayList.add(ans1);
 
                 }
-            } else {
-                for (int i = 1; i < n; i++) {
-                    int number = i + 1;
-                    System.out.println("1 " + i + " " + i + " " + number);
-                    int ans1 = scanner.nextInt();
-                    System.out.println("1 " + i + " " + number + " " + number);
-                    int ans0 = scanner.nextInt();
-                    arrayList.add(i - 1, ans0);
-                    arrayList.add(i, ans1);
-                    i++;
-                }
-                System.out.println("1 " + n + " " + n + " " + n);
-                int ans = scanner.nextInt();
-                arrayList.add(n - 1, ans);
-
-            }
-
 
             System.out.print("2 ");
             for (int i = 0; i < arrayList.size(); i++) {
